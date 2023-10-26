dfbe2ad528c24e1d18edef6344716c77625fdcd0
amane1234 <amane12@outlook.kr>
2023-10-25 22:31:24 -0400

    First

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from 'next/image';
 import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
 import { SignInButton } from "@clerk/clerk-react";
@@ -14,10 +15,10 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Jotion</span>
+        Your <span className="underline">wiki</span>, <span className="underline">docs</span>, & <span className="underline">projects</span>. Together.
       </h1>
-      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-        Jotion is the connected workspace where <br />
+      <h3 className="mt-2 text-base sm:text-xl md:text-2xl font-medium">
+        Notion is the connected workspace where <br />
         better, faster work happens.
       </h3>
       {isLoading && (
@@ -28,7 +29,7 @@ export const Heading = () => {
       {isAuthenticated && !isLoading && (
         <Button asChild>
           <Link href="/documents">
-            Enter Jotion
+            Enter Notion
             <ArrowRight className="h-4 w-4 ml-2" />
           </Link>
         </Button>
@@ -36,7 +37,7 @@ export const Heading = () => {
       {!isAuthenticated && !isLoading && (
         <SignInButton mode="modal">
           <Button>
-            Get Jotion free
+            Get Notion for free
             <ArrowRight className="h-4 w-4 ml-2" />
           </Button>
         </SignInButton>
